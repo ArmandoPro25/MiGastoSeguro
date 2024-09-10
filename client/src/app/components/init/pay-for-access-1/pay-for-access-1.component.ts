bf469392bbee9c744d85efded536222afd9e1f9c
ArmandoPro25 <ruanoarmando54@gmail.com>
2024-09-09 22:33:12 -0600

    Avance

--- client/src/app/components/init/pay-for-access-1/pay-for-access-1.component.ts
+++ client/src/app/components/init/pay-for-access-1/pay-for-access-1.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './pay-for-access-1.component.css'
 })
 export class PayForAccess1Component {
+  formatCardNumber(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\s+/g, '');
+    value = value.replace(/[^0-9]/gi, '');
 
+    const formattedValue = value.match(/.{1,4}/g)?.join(' ') || '';
+    input.value = formattedValue;
+  }
 }
